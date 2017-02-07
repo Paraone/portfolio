9158e696d3b55e543e53ec7e5b19d74507d5f7eb
Paraone <marcoswade@gmail.com>
2017-02-07 18:51:42 -0500

    porfolio mvp done

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,13 @@ class Contact extends React.Component{
 	render(){
 		return(
 			<div className="contents container">
-				This is my contact page...
+				<ul className="list-unstyled">
+					<li>Marcos Wade</li>
+					<li>marcoswade@gmail.com</li>
+					<li>347.927.2728</li>
+				</ul>
+				
+
 			</div>
 		)
 	}
